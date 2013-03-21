612521d8d8d9c63439f30c0a58203cf2f1b03630
tediscript <tediscript@gmail.com>
2013-03-21 10:59:28 +0700

    Initial Commit

---
+++ NavigationToy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavigationToy
+//
+//  Created by Tedi Fibri on 3/21/13.
+//  Copyright (c) 2013 tediscript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NavigationToy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NavigationToy
+//
+//  Created by Tedi Fibri on 3/21/13.
+//  Copyright (c) 2013 tediscript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
